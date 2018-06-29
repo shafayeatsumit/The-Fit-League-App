d0848c1bd700ca558427f47f12c9198edb902001
shafayeatsumit <shafayeatkabir@gmail.com>
2018-06-30 00:02:25 +0600

    intregated with backend notification settings component

--- app/components/NotificationSettings.js
+++ app/components/NotificationSettings.js
@@ -2,59 +2,65 @@ import React, { Component } from 'react';
 import { 
   Text, 
   View,
-  Image,
+  Alert,
+  ActivityIndicator,
   TouchableHighlight,
-  TextInput,
   Switch,
+  ScrollView,
   StyleSheet
 } from 'react-native';
 
+import { HttpUtils } from '../services/HttpUtils';
+
 const settings = [
   {
-    name: 'sundayReminder',
+    name: 'sunday_reminder',
     title: 'Sunday Reminder',
     subtitle: 'Weekly reminder to log workouts'
   },
   {
-    name: 'newMatchup',
+    name: 'new_matchup',
     title: 'New Matchup',
     subtitle: 'Monday announcement of new parter and opponents'
   },
   {
-    name: 'matchupStatus',
+    name: 'matchup_status',
     title: 'Matchup Status',
     subtitle: 'Daily updates on matchup "status"(winning, losing or tied)'
   },
   {
-    name: 'partnerWorkout',
+    name: 'partner_workout',
     title: 'Partner Workout',
     subtitle: 'Notified every time your partner logs a workout'
   },
   {
-    name: 'oponentWorkout',
+    name: 'opponent_workout',
     title: 'Opponent Workout',
     subtitle: 'Notified every time your opponent logs a workout'
   }
 ]
 
+
 class NotificationSettings extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: true,
       switches : {
-        sundayReminder: false,
-        newMatchup: false,
-        matchupStatus: false,
-        partnerWorkout: false,
-        oponentWorkout: false,
+        sunday_reminder: true,
+        new_matchup: true,
+        matchup_status: true,
+        partner_workout: true,
+        opponent_workout: true,
       }
     }
     this.switchHandler = this.switchHandler.bind(this);
+    this.handleSave = this.handleSave.bind(this);
+    this.loadProfile = this.loadProfile;
   }
 
   switchHandler(switchName) {
     const switchValue = this.state.switches[switchName];
-    // TODO: handle switches
     this.setState({
       switches: {
         ...this.state.switches,
@@ -63,24 +69,79 @@ class NotificationSettings extends Component {
     });
   }
 
+  loadProfile() {
+    HttpUtils.get('profile', this.props.token)
+      .then((responseData) => {
+        let { notification_preferences } = responseData.data.attributes
+        this.setState({
+          loading: false,
+          switches: {
+            ...notification_preferences
+          }
+        })        
+      }).catch((err) => {
+        // TODO: Nothing
+        this.setState({loading: false})
+    }).done()
+  }
+
+  handleSave() {
+    const { token } = this.props;
+    const { switches } = this.state;
+    const falseNotifications = {}
+    Object.keys(switches).forEach((key)=> {
+      if(switches[key] === false) {
+        falseNotifications[key] = false 
+      } 
+    })
+    this.setState({ loading: true })
+    HttpUtils.put('profile', { notification_preferences: switches }, token).then((response) => {
+      this.setState({ loading: false })
+      Alert.alert("YaY! Notification Update Successful.")  
+    }).catch((error) => {
+      this.setState({ loading: false })
+      Alert.alert("Sorry! Update failed.", error.message)
+    }).done()       
+  }
+
+  componentDidMount() {
+    this.loadProfile()  
+  }
+
   render() {
     return (
       <View style={styles.mainContainer}>
-        { settings.map((setting, i)=> {
-            const switchName = setting.name;
-            return(
-              <View style={styles.notificationSettings} key={i}>
-                <View style={styles.textContainer}>
-                  <Text style={styles.titleText}>{setting.title}</Text>
-                  <Text style={styles.subtitleText}>{setting.subtitle}</Text>
-                </View>
-                <View style={styles.switchContainer}>
-                  <Switch onValueChange={this.switchHandler.bind(this, switchName)} value={this.state.switches[switchName]}/>
-                </View>
-              </View>    
-            )
-          })
-        }
+        {
+          this.state.loading ? 
+          <View style={styles.loadingConainer}>
+            <ActivityIndicator size="large" style={styles.loading} color="black" />
+          </View>
+          :    
+          <View style={styles.mainContainer}>  
+          <ScrollView style={styles.settingsContainer} >
+            { settings.map((setting, i)=> {
+                const switchName = setting.name;
+                return(
+                  <View style={styles.notificationSettings} key={i}>
+                    <View style={styles.textContainer}>
+                      <Text style={styles.titleText}>{setting.title}</Text>
+                      <Text style={styles.subtitleText}>{setting.subtitle}</Text>
+                    </View>
+                    <View style={styles.switchContainer}>
+                      <Switch onValueChange={this.switchHandler.bind(this, switchName)} value={this.state.switches[switchName]}/>
+                    </View>
+                  </View>    
+                )
+              })
+            }
+          </ ScrollView>
+          <View style={styles.buttonContainer}>
+            <TouchableHighlight style={styles.saveButton} underlayColor='transparent' onPress={this.handleSave}>
+              <Text style={styles.saveButtonText}>Save</Text>
+            </TouchableHighlight>          
+          </View>
+          </View>
+        }                  
       </View>    
     );
   }
@@ -90,8 +151,17 @@ const styles = StyleSheet.create({
   mainContainer: {
     flex:1
   },
+  settingsContainer: {
+    flex:1,
+  },
+  loadingConainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },  
   notificationSettings: {
     flex:1,
+    padding: 5,
     flexDirection: 'row'
   },
   textContainer: {
@@ -110,7 +180,28 @@ const styles = StyleSheet.create({
     fontFamily: 'Avenir-Light',
     color: '#8691a0',
     fontSize: 16,
-  }
+  },
+  buttonContainer: {
+    //flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingVertical: 15,
+  },
+  saveButton: {
+    width: '50%',
+    height: 50,
+    borderRadius: 5,
+    backgroundColor:'#2C5CE9',
+    alignItems: 'center',
+    justifyContent: 'center' 
+  },
+  saveButtonText: {
+    fontFamily: 'Avenir-Black',
+    backgroundColor: 'transparent',
+    color: '#F1F4FD',
+    fontSize: 20,
+    padding: 5    
+  }    
 })
 
 export default NotificationSettings;
